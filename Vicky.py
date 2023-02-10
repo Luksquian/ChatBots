66e2d15fe5a47eace62cb9b222b0bed9c77a4ca6
Luksquian <123176896+Luksquian@users.noreply.github.com>
2023-02-10 10:34:31 -0300

    Update Vicky.py
     Se agrego libreria pyttsx3

--- Vicky.py
+++ Vicky.py
@@ -1,7 +1,9 @@
+# Las librerias deben importarse desde la consola o desde linea de comnado ejecutando pip install <libreria>
 import speech_recognition as sr
 import openai
 from gtts import gTTS
 from pygame import mixer
+import pyttsx3 # Si la libreria gTTS no funciona se puede utilizar esta que es la mas utilizada
 import os
 import time as ti
 import random
@@ -9,6 +11,8 @@ import random
 
 openai.api_key = "<YOUR APY KEY FROM OPENAI ACCOUNT>"
 
+
+# Definimos la funcion que transforma la voz captada en el mic a texto
 def transformar_audio_a_texto():
 
     r = sr.Recognizer()
@@ -17,7 +21,7 @@ def transformar_audio_a_texto():
         print("Ya puedes hablar!")
         audio = r.listen(origen)
         try:
-            pedido = r.recognize_google(audio, language="es-AR")
+            pedido = r.recognize_google(audio, language="es-AR") # Se debe especificar el lenguaje con el que se reconoce la voz
             print("You: " + pedido)
             return pedido
 
@@ -33,7 +37,10 @@ def transformar_audio_a_texto():
             print("Ups, algo salio mal!")
             return "Sigo esperando"
 
+        
+# Definimos la funcion que va a transformar el texto (mensaje) en audio, dejo tanto para libreria gTTS como para pyttsx3
 def hablar(mensaje):
+    # ******* Esta seccion de codigo es para libreria gTTS ********
     volume = 0.7
     tts = gTTS(mensaje, lang="es", slow=False)
     ran = random.randint(0,9999)
@@ -49,7 +56,17 @@ def hablar(mensaje):
 
     mixer.quit()
     os.remove(filename)
+    
+    # ******* Esta seccion de codigo es para libreria pyttsx3, solo utilizar una quitando el numeral ********
+    # En caso de que la libreria gTTS no funcione 
+    # Encender el motor de pyttsx3
+    #engine = pyttsx3.init()
+    # Bajar velocidad de reproduccion, por defecto es 200
+    #engine.setProperty('rate', 150)
 
+    # Pronunciar mensaje
+    #engine.say(mensaje)
+    #engine.runAndWait()
 
 def main():
     conversation = ""
